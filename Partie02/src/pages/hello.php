54e49bff4a5d0e6214103f70e3d0579ee4d3e7a6
chibsedu <chibsedu@gmail.com>
2023-02-07 17:59:25 +0100

    Cleansing for Partie02

--- Partie02/src/pages/hello.php
+++ Partie02/src/pages/hello.php
@@ -1,3 +1,3 @@
 
-<?php $name = $request->get('name', 'WorldTemplateING-ob') ?>
+<?php $name = $request->get('name', 'WorldTemplateING-ob Partie 02') ?>
 Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8')?>
